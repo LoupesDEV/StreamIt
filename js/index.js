44683c3250ee4b2b7181c705e23f612c72eacdfd
Kuco / Mathéo <85517097+KucoDEV@users.noreply.github.com>
2025-05-13 10:54:56 +0000

    💄 search bar & style update

--- js/index.js
+++ js/index.js
@@ -117,3 +117,10 @@ function filter() {
 
 loadSeriesData();
 loadFilmsData();
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "y") {
+    event.preventDefault();
+    document.getElementById("search-bar").focus();
+  }
+});
