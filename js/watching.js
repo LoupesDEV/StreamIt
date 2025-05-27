65e59c399e2ae0ac8214b479021c961a87102a51
Mathéo PICHOT-MOÏSE <matheo63450@gmail.com>
2025-05-27 13:44:02 +0200

    🔒️ no longer able to download videos

--- js/watching.js
+++ js/watching.js
@@ -275,4 +275,17 @@ function loadData() {
   }
 }
 
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+document.addEventListener('keydown', e => {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') ||
+    (e.ctrlKey && e.key.toLowerCase() === 'u')
+  ) {
+    e.preventDefault();
+  }
+});
+
+
 loadData();
