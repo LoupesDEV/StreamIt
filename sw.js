804144c7bc7a8d69347f73aabeb190119abe2d86
Mathéo PICHOT-MOÏSE <Matheo.PICHOT-MOISE@etu.uca.fr>
2025-12-22 19:30:05 +0100

    🔧 service worker update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "streamit-cache-v1";
-const urlsToCache = ["/", "/index.html", "/error.html", "/css/base.css", "/css/components.css", "/css/error.css", "/css/forms.css", "/css/layout.css", "/css/responsive.css", "/css/styles.css", "/css/tokens.css", "/css/variables.css", "/js/add.js", "/js/dataLoader.js", "/js/display.js", "/js/error.js", "/js/filters.js", "/js/init.js", "/js/modal.js", "/js/scripts.js", "/js/search.js", "/js/stats.js", "/js/storage.js", "/js/utils.js", "/medias/logo.png", "/data/films_data.json", "/data/series_data.json", "/data/collections.json"];
+const urlsToCache = ["/", "/index.html", "/error.html", "/css/error.css", "/css/responsive.css", "/css/style.css", "/css/variables.css", "/js/dataLoader.js", "/js/display.js", "/js/errorHandler.js", "/js/main.js", "/js/utils.js", "/medias/logo.png", "/data/films_data.json", "/data/series_data.json", "/data/collections.json"];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
