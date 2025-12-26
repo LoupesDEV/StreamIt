b31920fcbd139a7a3131c8899dc29fa004ff833d
Mathéo PICHOT-MOÏSE <Matheo.PICHOT-MOISE@etu.uca.fr>
2025-12-26 17:24:48 +0100

    🔧 added notifs.json to service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "streamit-cache-v1";
-const urlsToCache = ["/", "/index.html", "/error.html", "/css/error.css", "/css/responsive.css", "/css/style.css", "/css/variables.css", "/js/dataLoader.js", "/js/display.js", "/js/errorHandler.js", "/js/main.js", "/js/utils.js", "/medias/logo.png", "/data/films_data.json", "/data/series_data.json", "/data/collections.json"];
+const urlsToCache = ["/", "/index.html", "/error.html", "/css/error.css", "/css/responsive.css", "/css/style.css", "/css/variables.css", "/js/dataLoader.js", "/js/display.js", "/js/errorHandler.js", "/js/main.js", "/js/utils.js", "/medias/logo.png", "/data/films_data.json", "/data/series_data.json", "/data/collections.json", "/data/notifs.json"];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
